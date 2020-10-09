9aef831e0e07247c8186b55a545d7c18dd2f9d47
Alejandra Duran <alejandra.duran@synapse.com>
2020-10-08 23:09:50 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StarRatingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
